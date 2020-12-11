f0c72d43f9b217a4e5b161b66c85ed54add47481
The mark <sunduo3195@qq.com>
2020-12-11 11:31:03 +0800

    fix: 修改使用方式

--- app.js
+++ app.js
@@ -6,105 +6,114 @@ const middlewares = jsonServer.defaults();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.get("/test/country.json", (req, res) => {
-  res.send(router.db.get("test").get("country"));
-});
-server.post("/test/warning/queryReminder.json", (req, res) => {
-  console.log(req.body);
-  // let obj = router.db
-  // 	.get("test")
-  // 	.get("country")
-  // 	.find(req.body)
-  // 	.value()
-  // console.log(req.body.language)
-  // let newObj = Object.keys(obj)
-  // 	.filter(key => {
-  // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
-  // 			return key
-  // 		}
-  // 	})
-  // 	.reduce((_obj, key) => {
-  // 		_obj[key] = obj[key]
-  // 		return _obj
-  // 	}, {})
-  res.send(router.db.get("test").get("warning"));
-});
-server.post("/api/bill/query", (req, res) => {
-  console.log(req.body);
-  // let obj = router.db
-  // 	.get("test")
-  // 	.get("country")
-  // 	.find(req.body)
-  // 	.value()
-  // console.log(req.body.language)
-  // let newObj = Object.keys(obj)
-  // 	.filter(key => {
-  // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
-  // 			return key
-  // 		}
-  // 	})
-  // 	.reduce((_obj, key) => {
-  // 		_obj[key] = obj[key]
-  // 		return _obj
-  // 	}, {})
-  res.send(
-    router.db
-      .get("api")
-      .get("query_")
-  );
-});
-server.post("/test/warning/updateReminder.json", (req, res) => {
-  console.log(req.body);
 
-  // let obj = router.db
-  // 	.get("test")
-  // 	.get("country")
-  // 	.find(req.body)
-  // 	.value()
-  // console.log(req.body.language)
-  // let newObj = Object.keys(obj)
-  // 	.filter(key => {
-  // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
-  // 			return key
-  // 		}
-  // 	})
-  // 	.reduce((_obj, key) => {
-  // 		_obj[key] = obj[key]
-  // 		return _obj
-  // 	}, {})
-  res.send(
-    router.db
-      .get("test")
-      .get("country")
-      .find(req.body)
-      .value()
-  );
+server.post("/test/courseware/module/list", (req, res) => {
+  res.send(router.db.get("test").get("module").value()[0]);
 });
-server.post("/test/country.json", (req, res) => {
-  // let obj = router.db
-  // 	.get("test")
-  // 	.get("country")
-  // 	.find(req.body)
-  // 	.value()
-  // console.log(req.body.language)
-  // let newObj = Object.keys(obj)
-  // 	.filter(key => {
-  // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
-  // 			return key
-  // 		}
-  // 	})
-  // 	.reduce((_obj, key) => {
-  // 		_obj[key] = obj[key]
-  // 		return _obj
-  // 	}, {})
-  res.send(
-    router.db
-      .get("test")
-      .get("country")
-      .find(req.body)
-      .value()
-  );
+server.post("/test/courseware/module/add", (req, res) => {
+  res.json({
+    ret: 1,
+    data: {
+      moduleid: parseInt(Math.random() * 100000000000).toString(),
+    },
+  });
 });
+// server.post("/test/warning/queryReminder.json", (req, res) => {
+//   console.log(req.body);
+//   // let obj = router.db
+//   // 	.get("test")
+//   // 	.get("country")
+//   // 	.find(req.body)
+//   // 	.value()
+//   // console.log(req.body.language)
+//   // let newObj = Object.keys(obj)
+//   // 	.filter(key => {
+//   // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
+//   // 			return key
+//   // 		}
+//   // 	})
+//   // 	.reduce((_obj, key) => {
+//   // 		_obj[key] = obj[key]
+//   // 		return _obj
+//   // 	}, {})
+//   res.send(router.db.get("test").get("warning"));
+// });
+// server.post("/api/bill/query", (req, res) => {
+//   console.log(req.body);
+//   // let obj = router.db
+//   // 	.get("test")
+//   // 	.get("country")
+//   // 	.find(req.body)
+//   // 	.value()
+//   // console.log(req.body.language)
+//   // let newObj = Object.keys(obj)
+//   // 	.filter(key => {
+//   // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
+//   // 			return key
+//   // 		}
+//   // 	})
+//   // 	.reduce((_obj, key) => {
+//   // 		_obj[key] = obj[key]
+//   // 		return _obj
+//   // 	}, {})
+//   res.send(
+//     router.db
+//       .get("api")
+//       .get("query_")
+//   );
+// });
+// server.post("/test/warning/updateReminder.json", (req, res) => {
+//   console.log(req.body);
+
+//   // let obj = router.db
+//   // 	.get("test")
+//   // 	.get("country")
+//   // 	.find(req.body)
+//   // 	.value()
+//   // console.log(req.body.language)
+//   // let newObj = Object.keys(obj)
+//   // 	.filter(key => {
+//   // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
+//   // 			return key
+//   // 		}
+//   // 	})
+//   // 	.reduce((_obj, key) => {
+//   // 		_obj[key] = obj[key]
+//   // 		return _obj
+//   // 	}, {})
+//   res.send(
+//     router.db
+//       .get("test")
+//       .get("country")
+//       .find(req.body)
+//       .value()
+//   );
+// });
+// server.post("/test/country.json", (req, res) => {
+//   // let obj = router.db
+//   // 	.get("test")
+//   // 	.get("country")
+//   // 	.find(req.body)
+//   // 	.value()
+//   // console.log(req.body.language)
+//   // let newObj = Object.keys(obj)
+//   // 	.filter(key => {
+//   // 		if (new RegExp(req.body.language, "i").test(obj[key])) {
+//   // 			return key
+//   // 		}
+//   // 	})
+//   // 	.reduce((_obj, key) => {
+//   // 		_obj[key] = obj[key]
+//   // 		return _obj
+//   // 	}, {})
+//   res.send(
+//     router.db
+//       .get("test")
+//       .get("country")
+//       .find(req.body)
+//       .value()
+//   );
+// });
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 
